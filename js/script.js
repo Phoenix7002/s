dac6921c48d2710d5cae7514ba98a77bce3a75ae
Phoenix7002 <kkd2010rem@gmail.com>
2025-03-18 20:50:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const SECRET_TOKEN = "7x!A2d@L9z#Qw4e$Rt6y%Ui8o^Pp5s&Gh";
-
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.getElementById('navToggle');
     const navPanel = document.getElementById('navPanel');
