223f1963835aaae713d8e343d206530ac8a89c26
Phoenix7002 <kkd2010rem@gmail.com>
2025-03-19 15:30:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.getElementById('navToggle');
-    const navPanel = document.getElementById('navPanel');
+    const navToggle = document.querySelector('.main-nav-toggle');
+    const navPanel = document.querySelector('.main-nav-panel');
 
     navToggle.addEventListener('click', () => {
         navPanel.classList.toggle('open');
