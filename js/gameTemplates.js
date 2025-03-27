c96c8defb64288e55aee1d33e21873a3f3afb51c
Phoenix7002 <kkd2010rem@gmail.com>
2025-03-27 14:47:28 +0300

    Add files via upload

---
+++ js/gameTemplates.js
@@ -0,0 +1,58 @@
+const templates = [
+    `(div)
+    (p)
+        (lorem)
+    (/p)
+(/div)
+(span)
+    (lorem)
+(/span)`,
+
+    `(section)
+    (h1)
+        (lorem)
+    (/h1)
+    (p)
+        (lorem)
+    (/p)
+(/section)`,
+
+    `(article)
+    (h2)
+        (lorem)
+    (/h2)
+    (div)
+        (lorem)
+    (/div)
+(/article)`
+];
+
+const correctTags = {
+    'div': '<div>',
+    'p': '<p>',
+    'span': '<span>',
+    'section': '<section>',
+    'h1': '<h1>',
+    'h2': '<h2>',
+    'article': '<article>'
+};
+
+const incorrectTags = {
+    'div': ['<div/>', '<div', '</div>'],
+    'p': ['<p/>', '<p', '</p>'],
+    'span': ['<span/>', '<span', '</span>'],
+    'section': ['<section/>', '<section', '</section>'],
+    'h1': ['<h1/>', '<h1', '</h1>'],
+    'h2': ['<h2/>', '<h2', '</h2>'],
+    'article': ['<article/>', '<article', '</article>']
+};
+
+const loremTexts = [
+    "Lorem ipsum dolor sit amet.",
+    "Consectetur adipiscing elit.",
+    "Sed do eiusmod tempor incididunt.",
+    "Ut labore et dolore magna aliqua.",
+    "Ut enim ad minim veniam."
+];
+
+export { templates, correctTags, incorrectTags, loremTexts };
